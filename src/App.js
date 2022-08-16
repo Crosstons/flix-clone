f2392c16b9332f3ea1cae9f89cdeb989bec00582
Crostons <shubh1608003@gmail.com>
2022-08-16 13:20:52 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import request from "./request";
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BOOOOOOOOOH</h1>
+      <Row title="Originals" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Trending" fetchUrl={request.fetchTrending} /> 
+      
     </div>
   );
 }
