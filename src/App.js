c31dd50ce2956e15ecec773471ff474de991a5dd
Crostons <shubh1608003@gmail.com>
2022-08-16 19:15:56 +0530

    Another Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
 import './App.css';
-import Row from './Row';
-import request from "./request";
-import requests from './request';
+import Banner from './Components/Banner';
+import Navbar from './Components/Navbar';
+import Row from './Components/Row';
+import requests from './requests';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>BOOOOOOOOOH</h1>
-      <Row title="Originals" fetchUrl={request.fetchNetflixOriginals} />
-      <Row title="Trending" fetchUrl={request.fetchTrending} /> 
-      
+    <div className="app">
+
+      <Navbar />
+       
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow />
+
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+     
     </div>
   );
 }
